8ddbb9d03fa8925b0fe308b3b60f4760c2dd8a0b
Zzhiwei <zhiwei3141@gmail.com>
2022-08-26 22:20:25 +0800

    finish all tasks

--- nginx-sample/node-server/app.js
+++ nginx-sample/node-server/app.js
@@ -4,9 +4,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// const publicDirectoryPath = path.join(__dirname, "../public");
-// app.use(express.static(publicDirectoryPath));
-
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
